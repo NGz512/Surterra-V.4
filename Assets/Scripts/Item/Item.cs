a2c1384fff590347533ed67f3864ae0ecff68c62
Laylays <amklin_k@silpakorn.edu>
2022-12-16 17:14:34 +0700

    Testitem05(24)

--- Assets/Scripts/Item/Item.cs
+++ Assets/Scripts/Item/Item.cs
@@ -12,19 +12,32 @@ public class Item : MonoBehaviour
 
     private void Awake()
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
     }
 
     private void Start()
     {
-        if(ItemCode != 0)
+        if (ItemCode != 0)
         {
             Init(ItemCode);
         }
     }
 
-    public void Init (int itemCodePAram)
+    public void Init(int itemCodeParam)
     {
+        if (itemCodeParam != 0)
+        {
+            ItemCode = itemCodeParam;
 
+            ItemDetails itemDetails = InventoryManager.Instance.GetItemDetails(ItemCode);
+
+            spriteRenderer.sprite = itemDetails.itemSprite;
+
+            // If item type is reapable then add nudgeable component
+            if (itemDetails.itemType == ItemType.Reapable_scenary)
+            {
+                gameObject.AddComponent<ItemNudge>();
+            }
+        }
     }
 }

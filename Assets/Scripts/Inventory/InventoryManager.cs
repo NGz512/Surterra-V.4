5785e090414fb0c55cf3c81ad910cc72e5f15327
Laylays <amklin_k@silpakorn.edu>
2022-12-17 04:26:46 +0700

    TestUiitem08(27)

--- Assets/Scripts/Inventory/InventoryManager.cs
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -86,6 +86,7 @@ public class InventoryManager : SingletonMonobehaviour<InventoryManager>
         EventHandler.CallInventoryUpdatedEvent(inventoryLocation, inventoryLists[(int)inventoryLocation]);
     }
 
+
     /// <summary>
     /// Add item to the end of the inventory
     /// </summary>
@@ -97,7 +98,7 @@ public class InventoryManager : SingletonMonobehaviour<InventoryManager>
         inventoryItem.itemQuantity = 1;
         inventoryList.Add(inventoryItem);
 
-        DebugPrintInventoryList(inventoryList);
+        //DebugPrintInventoryList(inventoryList);
     }
 
     /// <summary>
@@ -113,7 +114,7 @@ public class InventoryManager : SingletonMonobehaviour<InventoryManager>
         inventoryList[position] = inventoryItem;
 
 
-        DebugPrintInventoryList(inventoryList);
+        //DebugPrintInventoryList(inventoryList);
     }
 
     /// <summary>
@@ -153,13 +154,13 @@ public class InventoryManager : SingletonMonobehaviour<InventoryManager>
         }
     }
 
-    private void DebugPrintInventoryList(List<InventoryItem> inventoryList)
-    {
-        foreach (InventoryItem inventoryItem in inventoryList)
-        {
-            Debug.Log("Item Description:" + InventoryManager.Instance.GetItemDetails(inventoryItem.itemCode).itemDescription + "    Item Quantity: " + inventoryItem.itemQuantity);
-        }
-        Debug.Log("******************************************************************************");
-    }
+    //private void DebugPrintInventoryList(List<InventoryItem> inventoryList)
+    //{
+    //    foreach (InventoryItem inventoryItem in inventoryList)
+    //    {
+    //        Debug.Log("Item Description:" + InventoryManager.Instance.GetItemDetails(inventoryItem.itemCode).itemDescription + "    Item Quantity: " + inventoryItem.itemQuantity);
+    //    }
+    //    Debug.Log("******************************************************************************");
+    //}
 
 }

61a432c10e87178d99a2f1eaa68db5fe5549baa5
Laylays2544 <amklin_k@silpakorn.edu>
2023-01-12 00:12:03 +0700

    UI_Shop(Test01)

---
+++ Assets/Scripts/UI/UIShop/UI_Shop.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UI_Shop : MonoBehaviour
+{
+    private Transform container;
+    private Transform shopItemTemplate;
+
+    private void Awake()
+    {
+        container = transform.Find("container");
+        shopItemTemplate = container.Find("shopItemTemplate");
+        shopItemTemplate.gameObject.SetActive(false);
+    }
+
+    private void CreateItemButton(Sprite itemSprite, string itemName, int itemCost, int positionIndex)
+    {
+        Transform shopItemTransform = Instantiate(shopItemTemplate, container);
+        shopItemTransform.gameObject.SetActive(true);
+        RectTransform shopItemRectTransform = shopItemTransform.GetComponent<RectTransform>();
+
+        float shopItemHeight = 90f;
+        shopItemRectTransform.anchoredPosition = new Vector2(0, -shopItemHeight * positionIndex);
+
+        shopItemTransform.Find("nameText").GetComponent<TextMeshProUGUI>().SetText(itemName);
+        shopItemTransform.Find("costText").GetComponent<TextMeshProUGUI>().SetText(itemCost.ToString());
+
+        shopItemTransform.Find("itemImage").GetComponent<Image>().sprite = itemSprite;
+    }
+}

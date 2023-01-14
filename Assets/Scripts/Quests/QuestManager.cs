48dea529233c3ff19570af3fef5142ca582fe4c3
Laylays2544 <amklin_k@silpakorn.edu>
2023-01-15 01:07:04 +0700

    Quest-03

--- Assets/Scripts/Quests/QuestManager.cs
+++ Assets/Scripts/Quests/QuestManager.cs
@@ -24,6 +24,44 @@ public class QuestManager : MonoBehaviour
         DontDestroyOnLoad(gameObject);
     }
 
+    public void QuestRequest(QuestObject NPCQuestObject)
+    {
+        //AVAILBLE QUEST
+        if (NPCQuestObject.availableQuestIDs.Count > 0)
+        {
+            for (int i = 0; i < questList.Count; i++)
+            {
+                for (int j = 0; j < questList.Count; j++)
+                {
+                    if(questList[i].id == NPCQuestObject.availableQuestIDs[j] && questList[i].progress == Quest.QuestProgress.AVAIABLE)
+                    {
+                        Debug.Log("Quest ID: " + NPCQuestObject.availableQuestIDs[j] + " " + questList[i].progress);
+                        //Testing
+                        AcceptQuest(NPCQuestObject.availableQuestIDs[j]);
+                        //Quest ui manager
+                    }
+                }
+            }
+        }
+
+        //ACTIVE QUESTS
+        for (int i = 0; i < currentQuestList.Count; i++)
+        {
+            for (int j = 0; j < NPCQuestObject.receivableQuestIDs.Count; j++)
+            {
+                if(currentQuestList[i].id == NPCQuestObject.receivableQuestIDs[j] && currentQuestList[i].progress == Quest.QuestProgress.ACCEPTED || currentQuestList[i].progress == Quest.QuestProgress.COMPLETE)
+                {
+                    Debug.Log("Quest ID: " + NPCQuestObject.receivableQuestIDs[j] + " is " + currentQuestList[i].progress);
+
+                    CompleteQuest(NPCQuestObject.receivableQuestIDs[j]);
+                    //Quest ui manager
+                }
+            }
+        }
+    }
+        
+        
+
     //ACCEPT QUEST
     public void AcceptQuest(int questID)
     {
@@ -60,6 +98,35 @@ public class QuestManager : MonoBehaviour
             {
                 currentQuestList[i].progress = Quest.QuestProgress.DONE;
                 currentQuestList.Remove(currentQuestList[i]);
+
+                //REWARD
+
+            }
+        }
+        //Check for chain quests
+        CheckChainQuest(questID);
+    }
+
+    //Check Chain Quest
+    void CheckChainQuest(int questID)
+    {
+        int tempID = 0;
+        for (int i = 0; i < questList.Count; i++)
+        {
+            if (questList[i].id == questID && questList[i].nextQuest > 0)
+            {
+                tempID = questList[i].nextQuest;
+            }
+        }
+
+        if (tempID > 0)
+        {
+            for (int i = 0; i < questList.Count; i++)
+            {
+                if (questList[i].id == tempID && questList[i].progress == Quest.QuestProgress.NOT_AVAILABLE)
+                {
+                    questList[i].progress = Quest.QuestProgress.AVAIABLE;
+                }
             }
         }
     }
@@ -83,7 +150,8 @@ public class QuestManager : MonoBehaviour
 
     //RemoveItem
 
-    // Bool
+    // BOOLS
+
     public bool RequestAvailableQuest(int questID)
     {
         for(int i = 0; i < questList.Count; i++)
@@ -115,6 +183,53 @@ public class QuestManager : MonoBehaviour
             if (questList[i].id == questID && questList[i].progress == Quest.QuestProgress.COMPLETE)
             {
                 return true;
+            }
+        }
+        return false;
+    }
+
+    // BOOLS 2
+
+    public bool CheckAvailableQuests(QuestObject NPCQuestObject)
+    {
+        for (int i = 0; i < questList.Count; i++)
+        {
+            for (int j = 0; j < NPCQuestObject.availableQuestIDs.Count; j++)
+            {
+                if (questList[i].id == NPCQuestObject.availableQuestIDs[j] && questList[i].progress == Quest.QuestProgress.AVAIABLE)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public bool CheckAcceptedQuests(QuestObject NPCQuestObject)
+    {
+        for (int i = 0; i < questList.Count; i++)
+        {
+            for (int j = 0; j < NPCQuestObject.receivableQuestIDs.Count; j++)
+            {
+                if (questList[i].id == NPCQuestObject.receivableQuestIDs[j] && questList[i].progress == Quest.QuestProgress.ACCEPTED)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public bool CheckCompletedQuests(QuestObject NPCQuestObject)
+    {
+        for (int i = 0; i < questList.Count; i++)
+        {
+            for (int j = 0; j < NPCQuestObject.receivableQuestIDs.Count; j++)
+            {
+                if (questList[i].id == NPCQuestObject.receivableQuestIDs[j] && questList[i].progress == Quest.QuestProgress.COMPLETE)
+                {
+                    return true;
+                }
             }
         }
         return false;
--- Assets/Scripts/Quests/QuestObject.cs
+++ Assets/Scripts/Quests/QuestObject.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class QuestObject : MonoBehaviour
 {
@@ -9,15 +10,50 @@ public class QuestObject : MonoBehaviour
     public List<int> availableQuestIDs = new List<int>();
     public List<int> receivableQuestIDs = new List<int>();
 
+    public GameObject questmarker;
+    public Image theImage;
+
+    public Sprite questAvailableSprite;
+    public Sprite questReceivableSprite;
+
     void Start()
     {
-
+        SetQuestMaker();
     }
+
+    void SetQuestMaker()
+    {
+        if (QuestManager.questManager.CheckCompletedQuests(this))
+        {
+            questmarker.SetActive(true);
+            theImage.sprite = questReceivableSprite;
+            theImage.color = Color.yellow;
+        }
+        else if (QuestManager.questManager.CheckAvailableQuests(this))
+        {
+            questmarker.SetActive(true);
+            theImage.sprite = questAvailableSprite;
+            theImage.color = Color.yellow;
+        }
+        else if (QuestManager.questManager.CheckAcceptedQuests(this))
+        {
+            questmarker.SetActive(true);
+            theImage.sprite = questReceivableSprite;
+            theImage.color = Color.gray;
+        }
+        else
+        {
+            questmarker.SetActive(false);
+        }
+    }
+
     private void Update()
     {
         if(inTrigger && Input.GetKeyDown(KeyCode.Space))
         {
             //quest ui manager
+
+            QuestManager.questManager.QuestRequest(this);
         }
     }
 

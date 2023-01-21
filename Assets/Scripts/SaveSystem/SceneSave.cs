eacaa5bad841467321f87741979402a322415f38
Laylays2544 <amklin_k@silpakorn.edu>
2023-01-21 15:08:41 +0700

    Saveposition

--- Assets/Scripts/SaveSystem/SceneSave.cs
+++ Assets/Scripts/SaveSystem/SceneSave.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 [System.Serializable]
 public class SceneSave
 {
-    public Dictionary<string, bool> boolDictionary; // string key is  an identifier name 
-    //string key is am identifier name we choose for this list
+    public Dictionary<string, bool> boolDictionary; //string key is am identifier name we choose for this list
+    public Dictionary<string, string> stringDictionary;
+    public Dictionary<string, Vector3Serializable> vector3Dictionary;
     public List<SceneItem> listSceneItem;
     public Dictionary<string, GridPropertyDetails> gridPropertyDetailsDictionary;
 }
